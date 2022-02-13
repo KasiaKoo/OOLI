67ef8157a0962e4e3408fd859ab4df6e555653ee
Kasia Kowalczyk <kmk216@ic.ac.uk>
2022-02-13 16:47:25 +0000

    add tests

--- test.py
+++ test.py
@@ -5,12 +5,15 @@ my_camera = Camera("Basler").initiate()
 print("Camera Params")
 print("-----------------")
 print(my_camera.gain)
+print(my_camera.gain_lower, my_camera.gain_upper)
 print(my_camera.exposure)
+print(my_camera.exposure_lower, my_camera.exposure_upper)
 print()
 
 my_stage = Stage("ThorLabs").initiate()
 print("Stage Params")
 print("-----------------")
 print(my_stage.position)
+print(my_stage.position_lower, my_stage.position_upper)
 
 
