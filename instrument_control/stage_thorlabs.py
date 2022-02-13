d974f9e2db748259787ef6ccbdf0b0d5e95a7ea9
Kasia Kowalczyk <kmk216@ic.ac.uk>
2022-02-13 16:43:49 +0000

    fix other getters

--- instrument_control/stage_thorlabs.py
+++ instrument_control/stage_thorlabs.py
@@ -11,14 +11,14 @@ class ThorLabsStage():
 
         self.position = self.get_position()
         self.position_lower, self.position_upper = self.get_position_limits()
-        self.moving = self.stage.is_in_motion()
+        self.moving = self.stage.is_in_motion
         self.hold = True
 
     def get_position(self):
         return self.stage.position
 
     def get_position_limits(self):
-        parameters = self.stage.get_stage_axis_info()
+        parameters = self.stage.get_stage_axis_info
         lower = parameters[0]
         upper = parameters[1]
         return lower, upper
