e6a030a23615ffe21420ec3e92eab78895bf5d9f
Kasia Kowalczyk <kmk216@ic.ac.uk>
2022-02-13 19:00:14 +0000

    bugfix

--- instrument_control/stage.py
+++ instrument_control/stage.py
@@ -9,12 +9,11 @@ class Stage:
         self.name = name
         self.model, self.stage_number = self.get_model("assets/stage_list.json")
 
+
     def initiate(self):
         if self.model == "ThorLabs":
             return ThorLabsStage(self.stage_number)
 
-        if self.model == "Smoract":
-            pass
 
     def get_model(self, stage_list):
         with open(stage_list) as f:
@@ -22,5 +21,6 @@ class Stage:
 
         model = stage_dict[self.name]["Model"]
         stage_number = stage_dict[self.name]["Stage Number"]
+        print(stage_number)
 
         return model, stage_number
